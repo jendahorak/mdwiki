fde5328d613fe967dd19ab69d85748fb46be3132
Jan Horák <7425857-shtrdl@users.noreply.gitlab.com>
2024-05-19 22:06:10 +0200

    delimit toc

--- utils/create_index.py
+++ utils/create_index.py
@@ -1,6 +1,9 @@
 import os
 
-def generate_index_md(root_folder, output_file="index.md"):
+def generate_index_md(root_folder):
+
+    output_file = os.path.join(root_folder, "index.md")
+
     with open(output_file, 'w', encoding='utf-8') as f:
         def write_folder_contents(folder_path, depth=2):
             # Get the folder name
@@ -16,12 +19,12 @@ def generate_index_md(root_folder, output_file="index.md"):
                     write_folder_contents(item_path, depth + 1)
                 elif item.endswith(".md"):
                     # Write the markdown file link
-                    f.write(f"[[{item}]]\n")
+                    f.write(f"[[{item}]]\n\n")
         
         # Start the recursive writing from the root folder
         write_folder_contents(root_folder)
 
 # Specify the root folder
-root_folder = "../content/"
+root_folder = r"D:\007_Projects\007_Programming\obsidian-publish\my_wiki\content"
 # Call the function to generate index.md
 generate_index_md(root_folder)
